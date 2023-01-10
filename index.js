087ec6b7e29cc554735bd63f5d7f020a0d7c1d10
Rosario Corres <rosariocorres@MBP-de-Rosario.fibertel.com.ar>
2023-01-09 22:07:40 -0300

    add initial static twits

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
 const express = require('express'); 
 const app = express(); 
+const path = require('path'); 
+const { v4: uuid} = require('uuid');
 
-app.set('view engine', 'ejs'); 
+app.use(express.static(path.join(__dirname, '/public'))); 
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));  
+
+app.use(express.urlencoded({ extended: true })); 
+app.use(express.json()); 
+
+//Array to mock a DB
+let twits = [
+    {
+        username: "Rick8102", 
+        twit: "messi scored!!", 
+        id: uuid()
+    }, 
+    {
+        username: "Marie", 
+        twit: "look at my cats, arent they cute?", 
+        id: uuid()
+    }, 
+    {
+        username: "PietroMax", 
+        twit: "oh wow", 
+        id: uuid()
+    }, 
+    {
+        username: "Magneto", 
+        twit: "I know crazy", 
+        id: uuid()
+    }
+]; 
 
 app.get('/', (req, res) => {
-    res.render('home'); 
+    res.render('home', { twits }); 
 })
+
 
 app.listen(3000, () => {
     console.log("listening on port 3000"); 
